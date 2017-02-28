f77c939ebb7efac0d00cc1db9612ef320605ef53
Travis Kemper <tkemper@Traviss-MacBook-Air.local>
2017-02-28 11:07:08 -0700

    Add setup.py for py2exe

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 28 11:02:07 2017
+
+@author: tkemper
+"""
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['quote.py'])
